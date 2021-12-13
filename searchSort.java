91fc2863cc3fcb6d91c3033dc7792bf62ba765c6
imadhyan <adhyanchawla500@gmail.com>
2021-12-13 08:55:57 +0530

    commit

--- searchSort.java
+++ searchSort.java
@@ -608,4 +608,201 @@ public class searchSort {
         return count;
     }
 
+    //lc 875
+    public int minEatingSpeed(int[] piles, int h) {
+        if(piles.length == 1) {
+            return (piles[0] % h != 0) ? piles[0] / h + 1 : piles[0] / h;    
+        }
+        
+        
+        Arrays.sort(piles);
+        int n = piles.length;
+        int hi = piles[n - 1];
+        int lo = 1;
+        // int mid = 0;
+        int ans = (int)1e9;
+        while(lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            int hours = 0;
+            for(int ele : piles) {
+                int div = 0, rem = 0;
+                // ele = (long)ele;
+                div = ele / mid;
+                rem = ele % mid;    
+                
+                if(rem != 0) {
+                    hours += div + 1;
+                } else {
+                    hours += div;
+                }
+            }
+             // System.out.println(hours);
+             if(hours <= h) {
+                 ans = mid;
+                 hi = mid - 1;
+            }
+             else {
+                lo = mid + 1;
+            }
+        }
+        
+        return ans;
+    }
+
+
+    //lc 1283
+    public int smallestDivisor(int[] nums, int threshold) {
+        int max = 0;
+        for(int ele : nums) {
+            max = Math.max(ele, max);
+        }
+        
+        int lo = 1;
+        int hi = max;
+        int ans = 0;
+        
+        while(lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            int div = 0;
+            int rem = 0;
+            int sum = 0;
+            for(int ele : nums) {
+                div = ele / mid;
+                rem = ele % mid;
+                
+                if(rem != 0) {
+                    sum += div + 1;
+                } else {
+                    sum += div;
+                }
+            }
+            
+            if(sum <= threshold) {
+                ans = mid;
+                hi = mid - 1;
+            } else lo = mid + 1;
+        }
+        
+        return ans;
+    }
+
+    // https://practice.geeksforgeeks.org/problems/find-the-element-that-appears-once-in-sorted-array0624/1#
+    int findOnce(int arr[], int n)
+    {
+        if(n == 1) return arr[0];
+        // Complete this function
+        if(n % 2 == 0) return 0;
+        int lo = 0;
+        int hi = n - 1;
+        
+        while(lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            
+            if(mid - 1 >= 0 && arr[mid - 1] != arr[mid] && mid + 1 < n && arr[mid + 1] != arr[mid]) {
+                return arr[mid];
+            }
+            
+            if(mid == 0) {
+                if(mid + 1 < n && arr[mid] != arr[mid + 1])
+                return arr[mid];
+            }
+            
+            if(mid == n - 1) {
+                if(mid - 1 >= 0 && arr[mid - 1] != arr[mid])
+                return arr[mid];
+            }
+            
+                
+            if(mid + 1 < n && mid - 1 >= 0 && arr[mid] == arr[mid + 1])
+                mid++;
+                
+            if((mid - lo + 1) % 2 == 0) {
+                lo = mid + 1;
+            } else hi = mid - 1;
+        }
+        
+        return 0;
+    }
+
+    //lc 475
+    public int findRadius(int[] houses, int[] heaters) {
+        Arrays.sort(heaters);
+        //Arrays.sort(houses);
+        //int n = houses.length;
+        int rad = 0;
+        for(int ele : houses) {
+            int f = floor(heaters, ele);
+            int c = ceil(heaters, ele);
+            f = (f == -1) ? Integer.MAX_VALUE : f;
+            c = (c == Integer.MAX_VALUE) ? Integer.MAX_VALUE : c;
+            int fdiff = (int)Math.abs(f - ele);
+            int cdiff = (int)Math.abs(c - ele);
+            int max = Math.min(fdiff, cdiff);
+            // System.out.println(max);
+            rad = Math.max(max, rad);
+        }
+        return rad;
+    }
+
+
+    // https://practice.geeksforgeeks.org/problems/punish-the-students5726/1#
+    public static int shouldPunish (int roll[], int marks[], int n, double avg)
+    {
+        int swaps = 0;
+        for(int i = 1; i < n; i++) {
+            for(int j = i; j < n - i; j++) {
+                if(roll[j] > roll[j + 1]) {
+                    swaps+=2;
+                    int temp = roll[j];
+                    roll[j] = roll[j + 1];
+                    roll[j + 1] = temp;
+                }
+            }
+        }
+        
+        double sum = 0;
+        for(int ele : marks) {
+            sum += ele;
+        }
+        
+        sum = (double)(sum - swaps);
+        sum = (double)(sum / n);
+        
+        return (sum >= avg) ? 1 : 0;
+    }
+
+    //lc 179
+    public String largestNumber(int[] nums) {
+        int n = nums.length;
+        String[] str = new String[n];
+        
+        for(int i = 0; i < n; i++) {
+            str[i] = nums[i] + "";
+        }
+        
+        Arrays.sort(str, (a, b) -> {
+           long n1 = Long.parseLong(a + b);
+           long n2 = Long.parseLong(b + a);
+           if(n1 > n2)
+               return 1;
+            else if(n1 < n2)
+                return -1;
+            else return 0;
+        });
+        
+        StringBuilder sb = new StringBuilder();
+        int i = n - 1;
+    
+        
+        while(i >= 0) {
+            sb.append(str[i]);
+            i--;
+        }
+        
+        if(sb.charAt(0) == '0')
+            return "0";
+        
+        return sb.toString() + "";
+    }
+
 }
